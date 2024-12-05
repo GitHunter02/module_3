2ad852242d1761509f08b87cc8c0898345ae6586
GitHunter02 <timur.ruslanovich25@gmail.com>
2024-12-06 03:10:19 +0500

    Add files via upload

--- module_3_14.py
+++ module_3_14.py
@@ -6,7 +6,7 @@ def single_root_words(root_word, *other_word):
     return same_words
 
 
-result1 = single_root_words('Rich', 'Rich', 'orichalcum', 'cheers', 'richies')
+result1 = single_root_words('riCh', 'rich', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 print(result1)
 print(result2)
