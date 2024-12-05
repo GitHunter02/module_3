061db74da331b27a73c279383884254fd1e60e70
GitHunter02 <timur.ruslanovich25@gmail.com>
2024-12-06 02:27:01 +0500

    модуль_3 - дз_5 - исправленное

--- module_3_17.py
+++ module_3_17.py
@@ -1,10 +1,16 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
-    if len(str_number) <= 1:
-        return int(str(number))
-    return first * get_multiplied_digits(int(str_number[1:]))
+    if len(str_number) == 1:
+        return first
+    elif first == 0:
+        return get_multiplied_digits(str_number[1:])
+    else:
+        return first * get_multiplied_digits(str_number[1:]) if first * get_multiplied_digits(
+            str_number[1:]) != 0 else first
 
 
-result = get_multiplied_digits(10000200003)
-print(result)
+result1 = get_multiplied_digits(40203)
+print(result1)
+result2 = get_multiplied_digits(402030)
+print(result2)
